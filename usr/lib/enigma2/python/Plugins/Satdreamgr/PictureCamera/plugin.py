5f65a85dd51d49cdafb565d84a5b0c41b7c82596
jpuigs <44906458+jpuigs@users.noreply.github.com>
2018-11-23 23:41:37 +0100

    Adapt plugin to work in 1920x1080 skins

--- usr/lib/enigma2/python/Plugins/Satdreamgr/PictureCamera/plugin.py
+++ usr/lib/enigma2/python/Plugins/Satdreamgr/PictureCamera/plugin.py
@@ -7,7 +7,7 @@ import os, urllib
 import requests
 from Screens.Screen import Screen
 from Plugins.Plugin import PluginDescriptor
-from enigma import ePicLoad, eTimer
+from enigma import ePicLoad, eTimer, getDesktop
 from Components.AVSwitch import AVSwitch
 from Components.Label import Label
 from Components.Pixmap import Pixmap
@@ -22,6 +22,9 @@ import urllib2
 from Screens.MessageBox import MessageBox 
 import shutil
 import gettext
+FULLHD = False
+if getDesktop(0).size().width() >= 1920:
+    FULLHD = True
 
 try:
 	cat = gettext.translation('Satdreamgr-Panel', '/usr/lib/enigma2/python/Plugins/Satdreamgr/Satdreamgr-Panel/locale', [config.osd.language.getText()])
@@ -38,24 +41,57 @@ def getCameras():
 		return cameras
 
 class PictureCamera(Screen):
-	skin = """
-	<screen name="PictureCamera" flags="wfNoBorder" position="0,0" size="1280,720" backgroundColor="transparent">
-    <widget name="title" render="Label" position="40,30" size="1210,60" backgroundColor="#50000000" zPosition="5" font="Regular; 28" valign="center" halign="center" noWrap="1" foregroundColor="#00f47d19" />
-	<widget name="menu" position="47,140" size="425,453" scrollbarMode="showOnDemand" halign="center" alphatest="on" backgroundColor="#20000000" font="Regular; 22"/>
-	<eLabel name="" position="40,90" size="437,554" zPosition="-2" backgroundColor="#20000000" />
+	if FULLHD:
+		skin = """
+	<screen name="PictureCamera" flags="wfNoBorder" position="center,center" size="1920,1080" backgroundColor="transparent">
+    <widget name="title" render="Label" position="40,30" size="1840,60" backgroundColor="#10000000" zPosition="5" font="Regular; 38" valign="center" halign="center" noWrap="1" foregroundColor="#00f47d19" />
+	<widget name="menu" position="47,155" size="498,770" scrollbarMode="showOnDemand" halign="center" alphatest="on" backgroundColor="#10000000" font="Regular; 30" itemHeight="35"/>
+	<eLabel name="" position="40,90" size="513,50" zPosition="-1" backgroundColor="#000064c7" />
+	<eLabel name="" position="40,140" size="513,800" zPosition="-2" backgroundColor="#10000000" />
+	<eLabel name="" position="40,940" size="513,50" zPosition="-1" backgroundColor="#000064c7" />
+	<widget name="pic" position="557,94" size="1323,892" halign="center" alphatest="on" backgroundColor="#10000000"/>
+	<eLabel name="" position="557,94" size="1323,892" zPosition="-1" backgroundColor="#10000000" />
+	<eLabel name="" position="40,990" size="460,10" zPosition="2" backgroundColor="#00ff4a3c" />
+    <eLabel name="" position="500,990" size="460,10" zPosition="2" backgroundColor="#00389416" />
+    <eLabel name="" position="960,990" size="460,10" zPosition="2" backgroundColor="#00e5b243" />
+    <eLabel name="" position="1420,990" size="460,10" zPosition="2" backgroundColor="#000064c7" />
+    <eLabel text="Exit" position="40,1000" size="460,50" zPosition="5" font="Regular;32" halign="center" valign="center" transparent="0" foregroundColor="#00fefefe" backgroundColor="#10000000" />
+    <eLabel text="Ok" position="500,1000" size="460,50" zPosition="5" font="Regular;32" halign="center" valign="center" transparent="0" foregroundColor="#00fefefe" backgroundColor="#10000000" />
+    <widget name="download" position="960,1000" zPosition="5" size="460,50" font="Regular;32" halign="center" valign="center" transparent="0" foregroundColor="#00fefefe" backgroundColor="#10000000" />
+    <eLabel text="Info" position="1420,1000" size="460,50" zPosition="5" font="Regular;32" halign="center" valign="center" transparent="0" foregroundColor="#00fefefe" backgroundColor="#10000000" />
+    <eLabel name="" position="40,990" size="1840,60" zPosition="1" backgroundColor="#10000000" />
+    <widget source="global.CurrentTime" render="Label" position="50,95" size="248,37" backgroundColor="#000064c7" transparent="1" font="Regular; 33" valign="center" halign="center" foregroundColor="#00fefefe" zPosition="2">
+      <convert type="ClockToText">Format:%A</convert>
+    </widget>
+    <widget source="global.CurrentTime" render="Label" position="305,95" size="248,37" backgroundColor="#000064c7" transparent="1" font="Regular; 33" valign="center" halign="center" foregroundColor="#00fefefe" zPosition="2">
+      <convert type="ClockToText">Format:%d-%m-%Y</convert>
+    </widget>
+    <widget source="global.CurrentTime" render="Label" position="140,940" size="165,52" backgroundColor="#000064c7" transparent="1" font="Regular; 48" halign="right" valign="center" zPosition="2" foregroundColor="#00fefefe">
+      <convert type="ClockToText">Format:%-H:%M</convert>
+    </widget>
+    <widget source="global.CurrentTime" render="Label" position="300,947" size="67,30" font="Regular; 30" halign="left" valign="center" transparent="1" backgroundColor="#000064c7" foregroundColor="#00f47d19" zPosition="2">
+      <convert type="ClockToText">Format: :%S</convert>
+    </widget>
+	 </screen>"""
+	else:
+		skin = """
+	<screen name="PictureCamera" flags="wfNoBorder" position="center,center" size="1280,720" backgroundColor="transparent">
+    <widget name="title" render="Label" position="40,30" size="1210,60" backgroundColor="#10000000" zPosition="5" font="Regular; 28" valign="center" halign="center" noWrap="1" foregroundColor="#00f47d19" />
+	<widget name="menu" position="47,140" size="425,453" scrollbarMode="showOnDemand" halign="center" alphatest="on" backgroundColor="#10000000" font="Regular; 22"/>
+	<eLabel name="" position="40,90" size="437,554" zPosition="-2" backgroundColor="#10000000" />
 	<eLabel name="" position="40,90" size="437,35" zPosition="-1" backgroundColor="#000064c7" />
 	<eLabel name="" position="40,609" size="437,35" zPosition="-1" backgroundColor="#000064c7" />
-	<widget name="pic" position="480,93" size="770,548" halign="center" alphatest="on" backgroundColor="#20000000"/>
-	<eLabel name="" position="480,93" size="770,548" zPosition="-1" backgroundColor="#20000000" />
+	<widget name="pic" position="480,93" size="770,548" halign="center" alphatest="on" backgroundColor="#10000000"/>
+	<eLabel name="" position="480,93" size="770,548" zPosition="-1" backgroundColor="#10000000" />
 	<eLabel name="" position="40,644" size="302,6" zPosition="2" backgroundColor="#00ff4a3c" />
     <eLabel name="" position="342,644" size="302,6" zPosition="2" backgroundColor="#00389416" />
     <eLabel name="" position="644,644" size="302,6" zPosition="2" backgroundColor="#00e5b243" />
     <eLabel name="" position="946,644" size="304,6" zPosition="2" backgroundColor="#000064c7" />
-    <eLabel text="Exit" position="40,650" size="302,50" zPosition="5" font="Regular;22" halign="center" valign="center" transparent="1" foregroundColor="#00fefefe" backgroundColor="#20000000" />
-    <eLabel text="Ok" position="342,650" size="302,50" zPosition="5" font="Regular;22" halign="center" valign="center" transparent="1" foregroundColor="#00fefefe" backgroundColor="#20000000" />
-    <widget name="download" position="644,650" zPosition="5" size="302,50" font="Regular;22" halign="center" valign="center" transparent="1" foregroundColor="#00fefefe" backgroundColor="#20000000" />   
-    <eLabel text="Info" position="946,650" size="302,50" zPosition="5" font="Regular;22" halign="center" valign="center" transparent="1" foregroundColor="#00fefefe" backgroundColor="#20000000" />
-	 <eLabel name="" position="40,644" size="1210,60" zPosition="1" backgroundColor="#50000000" />
+    <eLabel text="Exit" position="40,650" size="302,50" zPosition="5" font="Regular;22" halign="center" valign="center" transparent="1" foregroundColor="#00fefefe" backgroundColor="#10000000" />
+    <eLabel text="Ok" position="342,650" size="302,50" zPosition="5" font="Regular;22" halign="center" valign="center" transparent="1" foregroundColor="#00fefefe" backgroundColor="#10000000" />
+    <widget name="download" position="644,650" zPosition="5" size="302,50" font="Regular;22" halign="center" valign="center" transparent="1" foregroundColor="#00fefefe" backgroundColor="#10000000" />   
+    <eLabel text="Info" position="946,650" size="302,50" zPosition="5" font="Regular;22" halign="center" valign="center" transparent="1" foregroundColor="#00fefefe" backgroundColor="#10000000" />
+	 <eLabel name="" position="40,644" size="1210,60" zPosition="1" backgroundColor="#10000000" />
     <widget source="global.CurrentTime" render="Label" position="220,95" size="165,25" backgroundColor="#000064c7" transparent="1" font="Regular; 22" valign="center" halign="center" foregroundColor="#00fefefe" zPosition="2">
       <convert type="ClockToText">Format:%d-%m-%Y</convert>
     </widget>
