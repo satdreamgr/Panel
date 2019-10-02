214c368b0c9e0a0096154994f99adc8d4ab6e8c3
nautilus7 <spyros.n7@gmail.com>
2019-10-02 12:02:29 +0300

    [Backup plugin] Small cosmetics

--- usr/lib/enigma2/python/Plugins/Satdreamgr/SDGBackup/plugin.py
+++ usr/lib/enigma2/python/Plugins/Satdreamgr/SDGBackup/plugin.py
@@ -8,15 +8,17 @@ from Screens.MessageBox import MessageBox
 from Components.Label import Label
 from Components.config import config
 import gettext
+
+
 try:
 	cat = gettext.translation('Satdreamgr-Panel', '/usr/lib/enigma2/python/Plugins/Satdreamgr/Satdreamgr-Panel/locale', [config.osd.language.getText()])
 	_ = cat.gettext
 except IOError:
 	pass
 
+
 menu_s = "/usr/lib/enigma2/python/Plugins/Satdreamgr/SDGBackup/dreambox-fullbackup.sh"
 
-###########################################################################
 
 def main(session, **kwargs):
 	try:
@@ -24,21 +26,24 @@ def main(session, **kwargs):
 	except:
 		print "[SDGBackup] Plugin execution failed"
 
+
 def autostart(reason, **kwargs):
 	if reason == 0:
 		print "[PluginMenu] no autostart"
+
 
 def menu(menuid, **kwargs):
 	if menuid == "none":
 		return [(_("SDGBackup Dreambox Enigma2"), main, "sdgbackup_setup", 45)]
 	return []
 
+
 def Plugins(**kwargs):
 	return PluginDescriptor(name = _("SDGBackup Dreambox Enigma2"), description = _("SDGBackup Dreambox Enigma2"), where = PluginDescriptor.WHERE_MENU, fnc = menu)
 
-###########################################################################
 
 class SDGBackup(Screen):
+
 	skin = """
 		<screen position="center,center" size="460,400" title="SDGBackup Dreambox Enigma2">
 			<widget name="menu" position="10,10" size="420,380" scrollbarMode="showOnDemand"/>
@@ -54,8 +59,8 @@ class SDGBackup(Screen):
 		menu.append((_("Start SDGBackup on USB"), "/media/usb"))
 		menu.append((_("Start SDGBackup on HDD"), "/media/hdd"))
 		self.setTitle("SDGBackup Dreambox Enigma2")
-		self["key_red"] = self["myRedBtn"] = Label(_("Cancel")) # keep old button name for compatibility with skins
-		self["key_green"] = self["myGreenBtn"] = Label(_("OK")) # keep old button name for compatibility with skins
+		self["key_red"] = Label(_("Cancel"))
+		self["key_green"] = Label(_("OK"))
 		self["menu"] = MenuList(menu)
 		self["actions"] = ActionMap(["OkCancelActions", "SetupActions", "ColorActions", "WizardActions", "DirectionActions"], {"ok": self.go, "green": self.go, "cancel": self.close}, -1)
 
@@ -63,8 +68,8 @@ class SDGBackup(Screen):
 		returnValue = self["menu"].l.getCurrentSelection()[1]
 		if returnValue:
 			self.location = returnValue
-			self.session.openWithCallback(self.greek,MessageBox,_("You are about to create a backup to the selected location.\nDo you really want to proceed?"), MessageBox.TYPE_YESNO)
+			self.session.openWithCallback(self.greek, MessageBox, _("You are about to create a backup to the selected location.\nDo you really want to proceed?"), MessageBox.TYPE_YESNO)
 
 	def greek(self, answer):
 		if answer:
-			self.session.open(Console,_("Backup is running..."),["%s %s" % (menu_s, self.location)])
+			self.session.open(Console, _("Backup is running..."), ["%s %s" % (menu_s, self.location)])
